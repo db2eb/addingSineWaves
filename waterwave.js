c8ef483619943a2579269dd163231cd58b84bb8d
David Bang <34190869+db2eb@users.noreply.github.com>
2018-06-15 23:49:48 -0400

    Resetting moves the sliders back

--- waterwave.js
+++ waterwave.js
@@ -13,8 +13,8 @@ var context = canvas.get(0).getContext("2d"),
 var amplslider_1 = $("#ampl_1"), // sliders
     amplslider_2 = $("#ampl_2"),
     waveslider_1 = $("#waveWidth_1"),
-    waveslider_2 = $("#waveWidth_2");
-var colorString = 1,
+    waveslider_2 = $("#waveWidth_2"),
+	colorString = 1,
 	blackString = 3;
 
 var center_init = 0.15 * canvas.width,//where it starts 
@@ -23,7 +23,6 @@ var center_init = 0.15 * canvas.width,//where it starts
     yaxis1 = 250, //position in y axis
     animationSpeed = 50,//5000
     spread = 130, // Stick with this number
-    //speed = 2.85, // this is the least jerky speed
     delta_x = 0.015, // frequency
     delta_t = 0.05,
     step = .0005;
@@ -39,26 +38,10 @@ function init() {
 init();
 animate(animationSpeed); // short animation which stops after one draw
 
-
-//$("#reset").get(0).addEventListener("click", reset, false);
-
-// function reset(){
-// 	console.log("Hl");
-// 	//amplslider_1 = 30; wrong
-// 	amplslider_1 = document.getElementById("ampl_1").value = 30;
-//     //ampl_1 = Number(amplslider_1.val()); // allows editing after pausing
-
-
-//     center = center_init;
-//     wave_x = 10;
-//     controlpanel.pause = true;
-//     animate(animationSpeed); // short animation which stops after one draw
-//     $("#pause").text('Start');
-// }
-
 document.getElementById('reset').onclick = function(){
 	if(controlpanel.pause == false)
-		controlpanel.pause==true;
+		controlpanel.pause = true;
+    document.getElementById("test").reset();//resets sliders but not values
     ampl_1 = document.getElementById('ampl_1').value = 60;
     ampl_2 = document.getElementById('ampl_2').value = 60;
     waveWidth_1 = document.getElementById('waveWidth_1').value = 1;
@@ -75,33 +58,25 @@ function addEventlisteners() {
             $(this).text('Pause');
             animate(animationSpeed);
         } 
-        else { // click pause
+        else if (controlpanel.pause==false){ // click pause
             controlpanel.pause = true;
             $(this).text('Start');
         }
     });
+
     $("#stepback").bind('click', function(event) {
         controlpanel.pause = true;
         center = center - step;
         animate(animationSpeed);
         $("#pause").text('Start');
     });
+
     $("#stepforward").bind('click', function(event) {
         controlpanel.pause = true;
         center = center + step;
         animate(animationSpeed);
         $("#pause").text('Start');
     });
-
-    //======
-    // $("#reset").bind('click', function(event) {
-    //     center = center_init;
-    //     wave_x = 10;
-    //     controlpanel.pause = true;
-    //     animate(animationSpeed); // short animation which stops after one draw
-    //     $("#pause").text('Start');
-    // });
-    //======
 
     amplslider_1.bind('change', function(event) { //ampl_1 is green string
         if (controlpanel.pause) { // Changed from !controlpanel.started,
@@ -138,7 +113,6 @@ function animate(ms) {
         PulseCollisionDraw(yaxis1, ampl_2, 1);
     } else { 
         controlpanel.pause = true;
-        //controlpanel.started = false;
     }
     if (!controlpanel.pause) {
         setTimeout(animate, 10);
@@ -151,10 +125,8 @@ function wavey(A,k_1,wave_x,omega_1,delta_t) { // Math part
 
 function PulseCollisionDraw(yaxis, ampl2, visible) {
 	// Green String
-    //context.strokeStyle = "rgba(0, 255, 0)";//doesn't work in some browsers
     context.strokeStyle = "#007700";
     context.lineWidth=colorString;
-
     context.beginPath();
     for (i = 0; i < (reflector / delta_x); i++) {
         wave_x = i + delta_x;
@@ -164,10 +136,8 @@ function PulseCollisionDraw(yaxis, ampl2, visible) {
     context.stroke();
 
     // Red String
-    //context.strokeStyle = "rgba(255, 0, 0)";
     context.strokeStyle = "#ff0000";
-    context.lineWidth=colorString;
-
+    context.lineWidth = colorString;
     context.beginPath();
     for (i = 0; i < (reflector / delta_x); i++) {
         wave_x = i + delta_x;
@@ -177,13 +147,13 @@ function PulseCollisionDraw(yaxis, ampl2, visible) {
     context.stroke();
 
     // Black String
-    //context.strokeStyle = "rgba(0, 0, 0)";
     context.strokeStyle = "#000000";
-    context.lineWidth=blackString;
+    context.lineWidth = blackString;
     context.beginPath();
     for (i = 0; i < (reflector / delta_x); i++) {
         wave_x = i + delta_x;
-        wave_y = wavey(ampl_1, spread, center, waveWidth_1*wave_x, delta_t) + wavey(ampl_2, -spread, center, waveWidth_2*wave_x, delta_t);
+        wave_y = wavey(ampl_1, spread, center, waveWidth_1*wave_x, delta_t) 
+        	+ wavey(ampl_2, -spread, center, waveWidth_2*wave_x, delta_t);
         context.lineTo(wave_x, yaxis + wave_y);
     }
     context.stroke();
